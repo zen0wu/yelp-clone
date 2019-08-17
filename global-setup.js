8623388597ecd4083f221c8aaa6ff7a87fc44dbf
Luke <wucy@real.dev>
2019-08-16 23:45:59 -0700

    Add test

---
+++ global-setup.js
@@ -0,0 +1,10 @@
+const { setup: setupDevServer } = require("jest-dev-server");
+
+module.exports = async function globalSetup() {
+  await setupDevServer({
+    command: `yarn start`,
+    launchTimeout: 50000,
+    port: 3000,
+    verbose: true
+  });
+};
--- src/containers/App.test.js
+++ src/containers/App.test.js
@@ -1,9 +1,34 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import puppeteer from "puppeteer";
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+const RESTAURANT_NAME =
+  "Jack in the Box, 39017 Cedar Boulevard, Newark, CA, USA";
+
+let browser;
+let page;
+
+beforeAll(async () => {
+  browser = await puppeteer.launch();
+  page = await browser.newPage();
+  await page.goto("http://localhost:3000/");
+  page.setViewport({ width: 1920, height: 1080 });
+});
+
+afterAll(async () => {
+  await browser.close();
+});
+
+test("should render list of restaurants in both list and map views", async () => {
+  await page.focus("input");
+  await page.keyboard.type(RESTAURANT_NAME);
+  await page.keyboard.press("Enter");
+  await page.waitForResponse(
+    response => response.url().indexOf("/v3/businesses/search") !== -1
+  );
+
+  expect(
+    await page.evaluate(() => document.querySelector("#map-view").innerHTML)
+  ).toMatch("Jack in the Box");
+  expect(
+    await page.evaluate(() => document.querySelector("#list-view").innerHTML)
+  ).toMatch("Jack in the Box");
 });
