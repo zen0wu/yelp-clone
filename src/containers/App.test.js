2145355e4342a697d3015dfe307c022123b40b81
Zeno <shivaw@gmail.com>
2019-08-23 18:43:51 -0700

    Add proper timeout and correctly wait for text to appear

--- src/containers/App.test.js
+++ src/containers/App.test.js
@@ -1,21 +1,24 @@
 import puppeteer from "puppeteer";
 
+const TIMEOUT = 30000;
 const RESTAURANT_NAME =
   "Jack in the Box, 39017 Cedar Boulevard, Newark, CA, USA";
 
 let browser;
 let page;
 
-beforeAll(async () => {
-  browser = await puppeteer.launch();
+beforeAll(async (done) => {
+  browser = await puppeteer.launch({ args: ["--no-sandbox"] });
   page = await browser.newPage();
   await page.goto("http://localhost:3000/");
   page.setViewport({ width: 1920, height: 1080 });
-});
+  done();
+}, TIMEOUT);
 
-afterAll(async () => {
+afterAll(async (done) => {
   await browser.close();
-});
+  done();
+}, TIMEOUT);
 
 test("should render list of restaurants in both list and map views", async () => {
   await page.focus("#search-box");
@@ -25,10 +28,7 @@ test("should render list of restaurants in both list and map views", async () =>
     response => response.url().indexOf("/v3/businesses/search") !== -1
   );
 
-  expect(
-    await page.evaluate(() => document.querySelector("#map-view").innerHTML)
-  ).toMatch("Jack in the Box");
-  expect(
-    await page.evaluate(() => document.querySelector("#list-view").innerHTML)
-  ).toMatch("Jack in the Box");
-});
+  await page.waitForFunction(() => document.querySelector("#list-view").innerHTML.indexOf('Jack in the Box') !== -1)
+  await page.waitForFunction(() => document.querySelector("#map-view").innerHTML.indexOf('Jack in the Box') !== -1)
+}, TIMEOUT);
+
